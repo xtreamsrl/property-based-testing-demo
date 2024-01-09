e5b27a4f8848ff16c404cf570a2b2151ea2512e7
Fabio Lipreri <fabio.lipreri@xtreamers.io>
2024-01-09 17:22:02 +0100

    feat: go back to pizza demo starting point

--- src/PizzaGenerator.py
+++ src/PizzaGenerator.py
@@ -24,17 +24,12 @@ class Pizza:
     toppings: List[Ingredient]
     price: float
 
-    def __post_init__(self):
-        if math.isnan(self.price) | (not 0 <= self.price < float('inf')):
-            raise ValueError("Pizza price couldn't be NaN")
-
 
 class PizzaGenerator:
     def __init__(self, max_toppings: int = 3):
         self.max_toppings = max_toppings
 
     def generate_random_pizza(self, n_ingredients: int) -> Pizza:
-        n_ingredients = min(n_ingredients, self.max_toppings)
         toppings = [random.choice(list(Ingredient)) for _ in range(n_ingredients)]
         dough = random.choice(list(Dough))
         price = random.uniform(0, 30)
@@ -44,3 +39,51 @@ class PizzaGenerator:
 
 def calculate_order_total_price(order: List[Pizza]) -> float:
     return sum([pizza.price for pizza in order])
+
+
+
+#### DEMO SAVER!
+# class Ingredient(Enum):
+#     PEPPERONI = auto()
+#     PINEAPPLE = auto()
+#     MUSHROOMS = auto()
+#     POTATO = auto()
+#     GORGONZOLA = auto()
+#
+#
+# class Dough(Enum):
+#     NORMAL = auto()
+#     WHOLEMEAL = auto()
+#
+#
+# @dataclass
+# class Pizza:
+#     dough: Dough
+#     toppings: List[Ingredient]
+#     price: float
+#
+#     def __post_init__(self):
+#         if math.isnan(self.price) | (not 0 <= self.price < float('inf')):
+#             raise ValueError("Pizza price couldn't be NaN or Negative or Infinite")
+#
+#
+# class PizzaGenerator:
+#     def __init__(self, max_toppings: int = 3):
+#         if max_toppings < 0:
+#             raise ValueError("Max Toppings should be less than zero!")
+#         self.max_toppings = max_toppings
+#
+#     def generate_random_pizza(self, n_ingredients: int) -> Pizza:
+#         if n_ingredients < 0:
+#             raise ValueError("Number of ingredients should be less than zero!")
+#         n_ingredients = min(self.max_toppings, n_ingredients)
+#
+#         toppings = [random.choice(list(Ingredient)) for _ in range(n_ingredients)]
+#         dough = random.choice(list(Dough))
+#         price = random.uniform(0, 30)
+#
+#         return Pizza(dough, toppings, price)
+#
+#
+# def calculate_order_total_price(order: List[Pizza]) -> float:
+#     return sum([pizza.price for pizza in order])
--- tests/test_pizza_generator.py
+++ tests/test_pizza_generator.py
@@ -1,39 +1,86 @@
 import hypothesis.strategies as st
 import pytest
-from hypothesis import given, settings
+from hypothesis import given, settings, Verbosity
 
 from src.PizzaGenerator import calculate_order_total_price, Pizza, Dough, Ingredient, PizzaGenerator
 
 
-@given(st.integers(min_value=0), st.integers())
-@settings(max_examples=20)
-def test_generate_random_pizza_less_than_max_toppings(max_toppings: int, n_ingredients: int):
-    generated_pizza = PizzaGenerator(max_toppings).generate_random_pizza(n_ingredients)
+# @given(st.integers(), st.integers())
+# @settings(max_examples=50)
+# def test_generate_random_pizza_le_than_max_toppings(max_toppings: int, n_ingredients: int):
+#     generated_pizza = PizzaGenerator(max_toppings).generate_random_pizza(n_ingredients)
+#
+#     assert len(generated_pizza.toppings) <= max_toppings
+#
+#
+# @pytest.mark.parametrize("max_toppings, ingredients", [(-2, 3), (3, -2), (-4, -4)])
+# def test_negative_values_raise_an_error(max_toppings, ingredients):
+#     with pytest.raises(ValueError):
+#         _ = PizzaGenerator(max_toppings).generate_random_pizza(ingredients)
+#
+#
+# @st.composite
+# def order_composite(draw):
+#     n_pizza = draw(st.integers(min_value=0, max_value=100))
+#
+#     order = [Pizza(
+#         dough=draw(st.sampled_from(Dough)),
+#         toppings=draw(st.lists(st.sampled_from(Ingredient))),
+#         price=draw(st.floats())
+#     ) for _ in range(n_pizza)]
+#
+#     return order
+#
+#
+# @given(order_composite())
+# @settings(max_examples=20)
+# def test_correct_total_price(order):
+#     price = calculate_order_total_price(order)
+#     assert sum([pizza.price for pizza in order]) == price
+#
+#
+# @pytest.mark.parametrize('wrong_prices', [float('nan'), float('-inf'), float('+inf'), float('inf'), -10])
+# def test_raise_error_for_wrong_prices(wrong_prices):
+#     with pytest.raises(ValueError):
+#         Pizza(Dough.NORMAL, [Ingredient.POTATO, Ingredient.MUSHROOMS], wrong_prices)
 
-    assert len(generated_pizza.toppings) <= max_toppings
 
-
-@st.composite
-def order_composite(draw):
-    n_pizza = draw(st.integers())
-
-    order = [Pizza(
-        dough=draw(st.sampled_from(Dough)),
-        toppings=draw(st.lists(st.sampled_from(Ingredient))),
-        price=draw(st.floats(min_value=0, allow_nan=False, allow_infinity=False))
-    ) for _ in range(n_pizza)]
-
-    return order
-
-
-@given(order_composite())
-@settings(max_examples=20)
-def test_correct_total_price(order):
-    price = calculate_order_total_price(order)
-    assert sum([pizza.price for pizza in order]) == price
-
-
-@pytest.mark.parametrize('wrong_prices', [float('nan'), float('-inf'), float('+inf'), float('inf')])
-def test_raise_error_for_wrong_prices(wrong_prices):
-    with pytest.raises(ValueError):
-        Pizza(Dough.NORMAL, [Ingredient.POTATO, Ingredient.MUSHROOMS], wrong_prices)
+# DEMO SAVER
+# @given(st.integers(min_value=0, max_value=200), st.integers(min_value=0, max_value=200))
+# @settings(max_examples=50)
+# def test_generate_random_pizza_le_than_max_toppings(max_toppings: int, n_ingredients: int):
+#     generated_pizza = PizzaGenerator(max_toppings).generate_random_pizza(n_ingredients)
+#
+#     assert len(generated_pizza.toppings) <= max_toppings
+#
+#
+# @pytest.mark.parametrize("max_toppings, ingredients", [(-2, 3), (3, -2), (-4, -4)])
+# def test_negative_values_raise_an_error(max_toppings, ingredients):
+#     with pytest.raises(ValueError):
+#         _ = PizzaGenerator(max_toppings).generate_random_pizza(ingredients)
+#
+#
+# @st.composite
+# def order_composite(draw):
+#     n_pizza = draw(st.integers(min_value=0, max_value=100))
+#
+#     order = [Pizza(
+#         dough=draw(st.sampled_from(Dough)),
+#         toppings=draw(st.lists(st.sampled_from(Ingredient))),
+#         price=draw(st.floats(allow_infinity=False, allow_nan=False, min_value=0))
+#     ) for _ in range(n_pizza)]
+#
+#     return order
+#
+#
+# @given(order_composite())
+# @settings(max_examples=20)
+# def test_correct_total_price(order):
+#     price = calculate_order_total_price(order)
+#     assert sum([pizza.price for pizza in order]) == price
+#
+#
+# @pytest.mark.parametrize('wrong_prices', [float('nan'), float('-inf'), float('+inf'), float('inf'), -10])
+# def test_raise_error_for_wrong_prices(wrong_prices):
+#     with pytest.raises(ValueError):
+#         Pizza(Dough.NORMAL, [Ingredient.POTATO, Ingredient.MUSHROOMS], wrong_prices)
